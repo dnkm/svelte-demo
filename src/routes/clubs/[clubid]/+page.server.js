82d51aceadea39ac246e05f3879ac4693582a034
daniel kim <chipperday@gmail.com>
2025-01-10 15:39:06 -0800

    update

--- src/routes/clubs/[clubid]/+page.server.js
+++ src/routes/clubs/[clubid]/+page.server.js
@@ -1,10 +1,7 @@
-import { clubs } from '$lib/clubs';
-import { supabase } from '$lib/supabaseClient';
+// import { supabase } from '$lib/supabaseClient';
 
 export async function load({ params }) {
-  let { data } = await supabase.from("clubs").select("*").eq("id", params.clubid).maybeSingle()
+  //   let { data } = await supabase.from("clubs").select("*").eq("id", params.clubid).maybeSingle()
 
-  return ({
-    club: data
-  })
+  return { clubid: params.clubid }
 }
--- src/routes/clubs/[clubid]/+page.svelte
+++ src/routes/clubs/[clubid]/+page.svelte
@@ -1,9 +1,30 @@
 <script>
+	import { supabase } from '$lib/supabaseClient.js';
+
 	let { data } = $props();
+	let club = $state(null);
+
+	$effect(async () => {
+		if (data) {
+			let resp = await supabase.from('clubs').select('*').eq('id', data.clubid).single();
+			club = resp.data;
+		}
+	});
+
+	async function addLikes() {
+		// club.like++;
+		let resp = await supabase
+			.from('clubs')
+			.update({ like: club.like + 1 })
+			// .increment('like')
+			.eq('id', data.clubid)
+			.select('like')
+			.single();
+		club.like = resp.data.like;
+	}
 </script>
 
-<pre>{JSON.stringify(data)}</pre>
-<h1>{data.club?.id}</h1>
-
-<p>{data.club?.name}</p>
-<p>{data.club?.description}</p>
+<h1>club</h1>
+<pre>{club?.id}</pre>
+<pre>{club?.name}</pre>
+<button class="bg-gray-200 px-2 py-1" onclick={addLikes}>{club?.like}</button>
